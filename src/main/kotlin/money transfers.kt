6c3a418ad6345f2f2ac78a1308e222bcb54df74e
Sergey Bobkov <rex55@mail.ru>
2020-12-09 23:07:53 +0300

    v 1.2

--- src/main/kotlin/money transfers.kt
+++ src/main/kotlin/money transfers.kt
@@ -1,21 +1,20 @@
 fun main() {
-    var transferAmount = 11000000
-    var previousTransfers = 0
-    var limit = 15000000
-    var cardType = "MasterCard"
-    var commission  = feeAmount (cardType, transferAmount)
+    val transferAmount = 11000000
+    val previousTransfers = 0
+    val cardType = "MasterCard"
 
-        if ("Vk Pay" == cardType) limit = 1500000 else 15000000
-        if ((previousTransfers + transferAmount) > limit) println("Превышен лимит. Транзакция невозможна") else println("Коммиссия за перевод денежных средств составит: $commission копеек")
+    feeAmount (cardType, transferAmount, previousTransfers)
 }
-fun feeAmount(cardType: String, transferAmount: Int): Any {
+
+fun feeAmount(cardType: String, transferAmount: Int, previousTransfers: Int) {
     val sum= when (cardType) {
-        "Vk Pay" ->  0
-        "MasterCard" -> if (transferAmount < 7500000) 0 else (0.6 * transferAmount / 100 + 2000).toInt()
-        "Maestro" -> if (transferAmount < 7500000) 0 else (0.6 * transferAmount / 100 + 2000).toInt()
-        "Visa" -> (0.75 * transferAmount / 100).toInt()
-        "Мир" -> (0.75 * transferAmount / 100).toInt()
+        "MasterCard", "Maestro" -> if (transferAmount < 7500000) 0 else (0.6 * transferAmount / 100 + 2000).toInt()
+        "Visa", "Мир" -> (0.75 * transferAmount / 100).toInt()
         else -> 0
     }
-    return sum
+    
+    val limitDay =  if ("Vk Pay" == cardType)  1500000 else 15000000
+    val limitMonth = if ("Vk Pay" == cardType)  4000000 else 60000000
+    if (previousTransfers > limitMonth || transferAmount > limitDay) println("Превышен лимит. Транзакция невозможна") else println("Коммиссия за перевод денежных средств составит: $sum копеек")
+    val previousTransfers =+ transferAmount
 }
