6b5e700e8920b24b43db84183db89ff347aeb84b
Sergey Bobkov <rex55@mail.ru>
2020-12-13 17:31:14 +0300

    v 2.0 test

--- src/main/kotlin/money transfers.kt
+++ src/main/kotlin/money transfers.kt
@@ -3,10 +3,12 @@ fun main() {
     val previousTransfers = 0
     val cardType = "MasterCard"
 
-    feeAmount (cardType, transferAmount, previousTransfers)
+    val result = feeAmount (cardType, transferAmount, previousTransfers)
+
+
 }
 
-fun feeAmount(cardType: String, transferAmount: Int, previousTransfers: Int) {
+fun feeAmount(cardType: String, transferAmount: Int, previousTransfers: Int): Int {
     val sum= when (cardType) {
         "MasterCard", "Maestro" -> if (transferAmount < 7500000) 0 else (0.6 * transferAmount / 100 + 2000).toInt()
         "Visa", "Мир" -> (0.75 * transferAmount / 100).toInt()
@@ -17,4 +19,5 @@ fun feeAmount(cardType: String, transferAmount: Int, previousTransfers: Int) {
     val limitMonth = if ("Vk Pay" == cardType)  4000000 else 60000000
     if (previousTransfers > limitMonth || transferAmount > limitDay) println("Превышен лимит. Транзакция невозможна") else println("Коммиссия за перевод денежных средств составит: $sum копеек")
     val previousTransfers =+ transferAmount
+     return sum
 }
---
+++ src/test/kotlin/Money_transfersKtTest.kt
@@ -0,0 +1,85 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class Money_transfersKtTest{
+    @Test
+    fun feeAmountMaster() {
+        val transfer  = 11000000
+        val previous  = 0
+        val card  = "MasterCard"
+
+        val result= feeAmount(
+            cardType = card ,
+            previousTransfers = previous,
+            transferAmount = transfer
+        )
+        val expected = 68000
+
+        assertEquals(expected, result)
+    }
+    @Test
+    fun feeAmountVisa() {
+        val transfer  = 1100000
+        val previous  = 0
+        val card  = "Visa"
+
+        val result= feeAmount(
+            cardType = card ,
+            previousTransfers = previous,
+            transferAmount = transfer
+        )
+        val expected = 8250
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun feeAmountVkPay() {
+        val transfer  = 3000000
+        val previous  = 0
+        val card  = "Vk Pay"
+
+        val result= feeAmount(
+            cardType = card ,
+            previousTransfers = previous,
+            transferAmount = transfer
+        )
+        val expected =  0
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun feeAmountMaestro() {
+        val transfer  = 1100000
+        val previous  = 0
+        val card  = "Maestro"
+
+        val result= feeAmount(
+                cardType = card ,
+                previousTransfers = previous,
+                transferAmount = transfer
+        )
+        val expected =   0
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun feeAmountMir() {
+        val transfer  = 1000000
+        val previous  = 150000000
+        val card  = "Мир"
+
+        val result= feeAmount(
+                cardType = card ,
+                previousTransfers = previous,
+                transferAmount = transfer
+        )
+        val expected =  7500
+
+        assertEquals(expected, result)
+    }
+}
+
+
